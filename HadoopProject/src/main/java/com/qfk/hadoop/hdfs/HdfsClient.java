1a74ab1c411edbe925e57e404f86a6b02b5f294f
fukui.qi <fukui.qi@chinapex.com>
2020-12-22 17:31:25 +0800

    新增

--- HadoopProject/src/main/java/com/qfk/hadoop/hdfs/HdfsClient.java
+++ HadoopProject/src/main/java/com/qfk/hadoop/hdfs/HdfsClient.java
@@ -4,10 +4,9 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.junit.Test;
-
-
+import java.io.IOException;
 import java.net.URI;
-
+import java.net.URISyntaxException;
 
 public class HdfsClient {
     @Test
@@ -17,5 +16,32 @@ public class HdfsClient {
         fs.mkdirs(new Path("/qfk/hadoop/mkdirTest2"));
         fs.close();
         System.out.print("over");
+    }
+
+    @Test
+    public void testPut () throws Exception {
+        Configuration config = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop101:9000"),config,"qfk");
+        fs.copyFromLocalFile(new Path("D:\\hadoopExercise/text.txt"),new Path("/qfk/hadoop/input"));
+        fs.close();
+        System.out.print("over ...");
+    }
+
+    @Test
+    public void testGet () throws URISyntaxException, IOException, InterruptedException {
+        Configuration config = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop101:9000"),config,"qfk");
+        fs.copyToLocalFile(new Path("/qfk/hadoop/input/text.txt"),new Path("D:/"));
+        fs.close();
+        System.out.println("over!");
+    }
+
+    @Test
+    public  void testDel () throws URISyntaxException, IOException, InterruptedException {
+        Configuration config = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop101:9000"),config,"qfk");
+        fs.delete(new Path(""),true);
+        fs.close();
+        System.out.println("over success");
     }
 }
